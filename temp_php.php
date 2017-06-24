9814a6863532f90024eb3026ce29d3a60bdcf3e0
root <aameetpal_be15@thapar.edu>
2017-06-24 14:41:44 +0530

    added ajax for file transfer

--- temp_php.php
+++
@@ -1,4 +0,0 @@
-<?php
-	include("db.php");
-	print_r(query("select task_id,job_id from tasks"));
-?>
--- update.php
+++ update.php
@@ -3,22 +3,25 @@ include("db.php");
 include("any_process.php");
 
 try {
-	#$sql = "update workers set last_ping = now() where worker_ip = '".$_SERVER['REMOTE_ADDR']."'";
-	#update($sql);
-	#echo "PING DONE";
+	$sql = "update workers set last_ping = now() where worker_ip = '".$_SERVER['REMOTE_ADDR']."'";
+	update($sql);
+
 	$any_free_task = any_job();
 	if($any_free_task != null)
 	{
 	    $dir = "jobs/".$any_free_task[0]["job_id"];
 	    $worker_status_update = "update workers set status = 1 where worker_ip = '".$_SERVER['REMOTE_ADDR']."'";
-            $task_status_update = "update tasks set status = 1 where task_id = '".$_SERVER['REMOTE_ADDR']."'";
+            $task_status_update = "update tasks set status = 1 where task_id =".$any_free_task[0]["task_id"];
+	    $js_file = query("select process_func_file from jobs where = ".$any_free_task[0]["job_id"])[0]["process_func_file"];
 	    header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
-            header("Cache-Control: public"); // needed for internet explorer
+            header("Cache-Control: public"); 
             header("Content-Type: application/zip");
             header("Content-Transfer-Encoding: Binary");
             header("Content-Length:".filesize($attachment_location));
             header("Content-Disposition: attachment; filename=file.zip");
-            readfile($dir.$any_free_task[1]["task_id"]");
+	    readfile($dir.$any_free_task[0]["task_id"]);
+	    readfile();
+            readfile($dir.$any_free_task[0]["job_id"]);
             die();        
         } 
 	else 
